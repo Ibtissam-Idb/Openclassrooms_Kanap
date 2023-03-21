dcaeaf9a8883fe82ba9b9142431ac83fbfea519e
Ibtissam-Idb <idb.ibtissam@gmail.com>
2023-03-21 20:47:22 +0100

    fixed colors options problem

--- front/js/product.js
+++ front/js/product.js
@@ -5,18 +5,9 @@ const currentUrl = window.location.href;
 const url = new URL(currentUrl);
 const id = url.searchParams.get("id");
 
-console.log(id);
-
 const reponseApi = fetch("http://localhost:3000/api/products/" + id)
     .then(response => response.json())
     .then(result => generateFurnitures(result));
-
-/*const reponseApi = fetch("http://localhost:3000/api/products")
-    .then(response => response.json())
-    .then(result => generateFurnitures(result));
-
-const getFurnituresId = new URLSearchParams(reponseApi);
-const ParamsFurnituresId = getFurnituresId.get("id");*/
 
 // End
 
@@ -40,16 +31,18 @@ function generateFurnitures(furnitures) {
         const furnitureDescription = document.querySelector("#description");
         furnitureDescription.innerText = article.description;
 
-        const furnitureSelect = document.querySelector("#colors");
         const furnitureColors = article.colors;
 
-        for (let color; color < furnitureColors.length; color++) {
-            const colorOption = furnitureColors[color];
-            const furnitureOptions = document.createElement("option");
-            
-            furnitureOptions.innerHTML = colorOption;
-            furnitureOptions.value = colorOption;
+        for(let i = 0; i < furnitureColors.length; i++) {
 
-            furnitureSelect.appendChild(furnitureOptions);
+            const color = furnitureColors[i];
+
+            const furnitureSelect = document.getElementById("colors");
+            const furnitureOption = document.createElement("option");
+
+            furnitureOption.innerHTML = color;
+            furnitureOption.value = color;
+
+            furnitureSelect.appendChild(furnitureOption);
         }
 }
