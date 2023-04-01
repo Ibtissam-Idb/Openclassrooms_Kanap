85bb91c38f3a630e9925d5a683ef64c20d49ea59
Ibtissam-Idb <idb.ibtissam@gmail.com>
2023-04-01 18:55:52 +0200

    add to cart (must add no-dupplicate condition)

--- front/js/product.js
+++ front/js/product.js
@@ -3,46 +3,77 @@
 
 const currentUrl = window.location.href;
 const url = new URL(currentUrl);
-const id = url.searchParams.get("id");
+const productId = url.searchParams.get("id");
 
-const reponseApi = fetch("http://localhost:3000/api/products/" + id)
+const reponseApi = fetch("http://localhost:3000/api/products/" + productId)
     .then(response => response.json())
     .then(result => generateFurnitures(result));
 
 // End
 
 // Generate Correct Product
-    
+
 function generateFurnitures(furnitures) {
-        const article = furnitures;
+    const article = furnitures;
 
-        const imageContainer = document.querySelector(".item__img");
-        const furnitureImage = document.createElement("img");
-        furnitureImage.src = article.imageUrl;
-        furnitureImage.alt = article.altTxt;
-        imageContainer.appendChild(furnitureImage);
+    const imageContainer = document.querySelector(".item__img");
+    const furnitureImage = document.createElement("img");
+    furnitureImage.src = article.imageUrl;
+    furnitureImage.alt = article.altTxt;
+    imageContainer.appendChild(furnitureImage);
 
-        const furnitureTitle = document.querySelector("#title");
-        furnitureTitle.innerText = article.name;
+    const furnitureTitle = document.querySelector("#title");
+    furnitureTitle.innerText = article.name;
 
-        const furniturePrice = document.querySelector("#price");
-        furniturePrice.innerText = article.price;
+    const furniturePrice = document.querySelector("#price");
+    furniturePrice.innerText = article.price;
 
-        const furnitureDescription = document.querySelector("#description");
-        furnitureDescription.innerText = article.description;
+    const furnitureDescription = document.querySelector("#description");
+    furnitureDescription.innerText = article.description;
 
-        const furnitureColors = article.colors;
+    const furnitureColors = article.colors;
 
-        for(let i = 0; i < furnitureColors.length; i++) {
+    for (let i = 0; i < furnitureColors.length; i++) {
 
-            const color = furnitureColors[i];
+        const color = furnitureColors[i];
 
-            const furnitureSelect = document.getElementById("colors");
-            const furnitureOption = document.createElement("option");
+        const furnitureSelect = document.getElementById("colors");
+        const furnitureOption = document.createElement("option");
 
-            furnitureOption.innerHTML = color;
-            furnitureOption.value = color;
+        furnitureOption.innerHTML = color;
+        furnitureOption.value = color;
 
-            furnitureSelect.appendChild(furnitureOption);
-        }
+        furnitureSelect.appendChild(furnitureOption);
+    }
 }
+
+// End
+
+// Add data to Local Storage
+
+const productQuantity = document.getElementById("quantity");
+const productColors = document.getElementById("colors");
+
+const addToCartButton = document.getElementById("addToCart");
+
+addToCartButton.addEventListener("click", function(event) {
+    let newItem = {
+        id: productId,
+        color: productColors.value,
+        quantity: productQuantity.value,
+    }
+
+    const getItems = JSON.parse(localStorage.getItem("items")) || [];
+
+    const findSameItems = getItems.findIndex((item) => newItem.id === item.id && newItem.color === item.color);
+
+    if (findSameItems === -1) {
+        getItems.push(newItem);
+        localStorage.setItem("items", JSON.stringify(getItems));
+    } else {
+        item.quantity += newItem.quantity; // error: item is not defined. 
+        getItems.push(newItem);
+        localStorage.setItem("items", JSON.stringify(getItems));
+    }
+
+});
